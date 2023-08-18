01261ae0f68cc2d994e23a5a543e71b42d1e38d9
Umskie <umarfarooqkhalid@gmail.com>
2023-08-17 21:00:40 -0500

    routing1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+import {PostsView, Unauthenticated} from "./components/PostAuthentication"
+import {PostsView, UpgradedwithAuthentication} from  "./components/PostUnauthenticated"
+import {RegisterLoginLogoutAuthentication} from "./components/Login-Logout"
+
 
 function App() {
   const [count, setCount] = useState(0)
