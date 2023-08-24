f69b9dbd625dd425a0d829d6042a9bf1a8ccd8ac
lngounou <laurangounou@yahoo.com>
2023-08-23 23:13:34 -0400

    Fixed logout function and auth variable. Updated nav headers.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,37 +4,38 @@ import '../App.css';
 
 
 const Navbar=()=> {
-    const auth = sessionStorage.getItem('user');
-    const navigate =useNavigate();
-    const logout =()=>{
+    const auth = sessionStorage.getItem('token');
+    console.log(auth)
+    const navigate = useNavigate();
+    const logout =()=> {
         sessionStorage.clear();
         navigate('/Register')
     }
+
+    if (auth) {
     return (
         <div>
-            
-        
-      
         
                 <ul className="navbar">
-                <li>
-                <Link to={'/'}>Home</Link>
-                </li>
-                <li>
-                <Link to={'/posts'}>Posts</Link>
-                </li>
-                <li><Link onClick={logout} to='/logout'>Logout </Link> 
-                </li>
-                <li>
-                <Link to={'/login'}>Login</Link>
-                </li>
-                <li>
-                <Link to={'/Register'}>Register</Link>
-                </li>
-             </ul>
 
+                <li><Link to={'/'}>Home</Link></li>
+                <li><Link to={'/posts'}>Posts</Link></li>
+                <li><Link to={'/login'}>Login</Link></li>
+                <li>{ auth ? <Link onClick={logout} to='/Register'>Logout</Link> : <Link to="/Register">Logout</Link>}</li>
+
+             </ul>
+            
+             
         </div>
     )
-}
+            } else {
+                return (
+                    <ul className="navbar">
+                    <li><Link to={'/Register'}>Register</Link></li>
+                    <li><Link to={'/login'}>Login</Link></li>
+                 </ul>
+                )
+            }
+        }
 
 export default Navbar;
